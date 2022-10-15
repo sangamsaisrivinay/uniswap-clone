d9448e54e5f3c1c0c2c63f936d376ca12b72d373
sangamsaisrivinay <saisrivinayreddysangam@gmail.com>
2022-10-15 09:23:36 +0530

    2nd commit

--- packages/react-app/src/App.js
+++ packages/react-app/src/App.js
@@ -1,7 +1,38 @@
 import React from "react"
+import {useEthers} from "@usedapp/core"
+import styles from './styles'
+import {uniswapLogo} from './assets'
+import {Exchange,Loader,WalletButton} from './components'
 
 const App = () => {
-  return <p>Hello, Web3!</p>
+  const {account}= useEthers()
+  const poolsLoading=false;
+  return (
+    <div className={styles.container}>
+      <div className={styles.innerContainer}>
+        <header className={styles.header}>
+          <img src={uniswapLogo} alt='logo' 
+          className="w-16 h-16 object-contain"/>
+          <WalletButton/>
+        </header>
+        <div className={styles.exchangeContainer}>
+          <h1 className={styles.headTitle} style={{color:'white'}}>Uniswap 2.0</h1>
+          <p className={styles.subTitle}>Trade tokens in an instant</p>
+          <div className={styles.exchangeBoxWrapper}>
+            <div className={styles.exchangeBox}>
+              <div className="pink_gradient"/>
+              <div className={styles.exchange}>
+                  {account?(
+                    poolsLoading?(<Loader title="Loading pools, please wait!"/>):<Exchange/>
+                  ):<Loader title="please connect your wallet!"/>}
+              </div>
+              <div className="blue_gradient"/>
+              </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default App;
--- packages/react-app/src/config.js
+++ packages/react-app/src/config.js
@@ -1,10 +1,10 @@
 import { Goerli } from "@usedapp/core";
 
-export const ROUTER_ADDRESS = "[YOUR ADDRESS HERE]"; 
+export const ROUTER_ADDRESS = "0xDBBD0c116BDbd2F1586a711E05fE4B5526e209A2"; 
 
 export const DAPP_CONFIG = {
   readOnlyChainId: Goerli.chainId,
   readOnlyUrls: {
-    [Goerli.chainId]: "[YOUR URL HERE]",
+    [Goerli.chainId]: "https://eth-goerli.g.alchemy.com/v2/N10tYSNDkvkW0eu7FTdFV51gItQAsYYK",
   },
 };
